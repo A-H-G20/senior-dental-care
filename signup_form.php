434550be3dca9be51937003f7af578a9961e878e
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-08-14 22:14:56 +0300

    solve error

--- signup_form.php
+++ signup_form.php
@@ -6,12 +6,9 @@ include ('dbcon.php');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'C:\Users\Ahmad\phpmailer\vendor\phpmailer\phpmailer\src\PHPMailer.php';
-require 'C:\Users\Ahmad\phpmailer\vendor\phpmailer\phpmailer\src\Exception.php';
-require 'C:\Users\Ahmad\phpmailer\vendor\phpmailer\phpmailer\src\SMTP.php';
-
-require 'C:\Users\Ahmad\phpmailer\vendor\autoload.php';
-
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 if (isset($_POST['submit'])) {
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
